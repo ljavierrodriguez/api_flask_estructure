e830c309ef34ae8c95e4e32685f394ace7aa7544
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-06-27 16:53:36 -0400

    Using Context

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -11,6 +11,9 @@ def login():
     username = request.json.get('username')
     password = request.json.get('password')
     
+    if not username: return jsonify({ "username": "Username is required!"}), 400
+    if not password: return jsonify({ "password": "Password is required!"}), 400
+    
     userFound = User.query.filter_by(username=username).first()
     
     if not userFound: return jsonify({ "message": "username/password is incorrect"}), 401
@@ -18,7 +21,7 @@ def login():
     if not check_password_hash(userFound.password, password):
         return jsonify({ "message": "username/password is incorrect"}), 401
     
-    acces_token = create_access_token(identity=userFound.id)
+    acces_token = create_access_token(identity=userFound["id"])
     
     data = {
         "access_token": acces_token,
